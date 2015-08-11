95f6fd03a052d587031839b7e78890470410b387
Enide Dufresne <enide.dufresne@gmail.com>
2015-08-11 00:16:51 -0400

    lose ends

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require bootstrap
+//= require bindWithDelay
+//= require jquery.datetimepicker
 //= require_tree .
 
 $(document).ready(function(){
