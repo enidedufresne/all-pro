c2f61c53b3488bedbe08b99ed3e5ef12b042d782
Enide Dufresne <enide.dufresne@gmail.com>
2015-09-20 00:41:33 -0400

    commentable to models

--- app/models/catcher_stat.rb
+++ app/models/catcher_stat.rb
@@ -1,6 +1,13 @@
 class CatcherStat < ActiveRecord::Base
+  include Shared::Callbacks
   belongs_to :user
   belongs_to :stat
   belongs_to :sport
   belongs_to :position
+
+  acts_as_votable
+  acts_as_commentable
+
+  include PublicActivity::Model
+  tracked only: [:create], owner: Proc.new{ |controller, model| model.user }
 end
