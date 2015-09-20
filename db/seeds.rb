25f087ef026f6d1f55d905b66c8ec2690926742e
Enide Dufresne <enide.dufresne@gmail.com>
2015-09-19 20:10:21 -0400

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+puts "Importing sports..."
+CSV.foreach(Rails.root.join("sports.csv"), headers: true) do |row|
+  Sport.create! do |sport|
+    sport.id = row[0]
+    sport.name = row[1]
+  end
+end
+
+puts "Importing positions..."
+CSV.foreach(Rails.root.join("positions.csv"), headers: true) do |row|
+  Position.create! do |position|
+    position.position_name = row[0]
+    position.shorthand = row[1]
+    # position.type = row[2]
+    position.sport_id = row[3]
+    position.id = row[4]
+  end
+end
+
+puts "Importing statfields..."
+CSV.foreach(Rails.root.join("statfields.csv"), headers: true) do |row|
+  Statfield.create! do |statfield|
+    statfield.name = row[0]
+    statfield.shorthand = row[1]
+    statfield.sport_id = row[2]
+    statfield.position_id = row[3]
+  end
+end
