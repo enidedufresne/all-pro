aec2738b1ca40b50a62629860d374ebdca8a86a6
Enide Dufresne <enide.dufresne@gmail.com>
2015-10-25 18:51:45 -0400

    add cancan ability model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,7 +74,7 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :about, :avatar, :cover,
-                                 :sex, :dob, :location, :phone_number, :role, :roles)
+                                 :sex, :dob, :location, :phone_number)
   end
 
   def check_ownership
