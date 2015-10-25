1c4dd65b4d7d9d52e1e6eb9093373eb671a61f54
Enide Dufresne <enide.dufresne@gmail.com>
2015-10-25 18:49:18 -0400

    sign up sanitizer for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:name, :password_confirmation]
     devise_parameter_sanitizer.for(:sign_in) << [:email, :remember_me]
+    devise_parameter_sanitizer.for(:sign_up) << :role
   end
 
   include PublicActivity::StoreController
